9f94b7b1e7aae08234c1ab8b560b5fc0a9ac585d
carithers <carithers@163.com>
2025-09-08 14:26:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '86c50e3a-5b87-49dd-bd20-03c7f2735e52';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -7187,4 +7187,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
